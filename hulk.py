671661f50dc4c27a88c41ad99033cdb3e152c7bf
k4m35h <1234.kamesh@gmail.com>
2018-03-19 16:19:52 +0530

    Update hulk.py

--- hulk.py
+++ hulk.py
@@ -1,12 +1,12 @@
+import re
 data = '''
-[lang:php]
-    [config]
-        [terminate];[/terminate]
-        [template]<?php\n{code}[/template]
-    [config]
-    [assignment]
-        [default]${variable} = {value}[/default]
-        [ifelse]${variable} = ({condition})? {value} : {else}[/ifelse]
-    [/assignment]
-[/lang]
+[config]
+    [terminate];[/terminate]
+    [template]<?php\n{code}[/template]
+[/config]
+[assignment]
+    [default]${variable} = {value}[/default]
+    [ifelse]${variable} = ({condition})? {value} : {else}[/ifelse]
+[/assignment]
 '''
+print re.findall(r'\[([a-z]+)\]([^\0]+)\[/\1\]',data, re.M|re.I)
